c222f4770f9e1bcdf7ae68beed9bf7caa67ec04f
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-07 20:31:41 -0800

    added function to append weather data to page as well as creating button from save button and local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,30 +1,45 @@
 var apiKey = "9975a2d1ef4e7fb59bbba6eef797ea85"
 var buttonPress = $(".userSearch");
 var localKey = 0;
-var currentWeather = "";
-var fiveWeather = "";
 var cityButton = $(".cityButton");
+var userInput = $(".inputSearch").val().trim();
+
+// for loop appends city from search and local storage as a button
+// for (var i = 0; i < localStorage.length; i++) {
 
 
-
-// for loop appends to search for a city section
-for (var i = 0; i < localStorage.length; i++) { 
-    var storedCity = localStorage.getItem(i);
-    $(".pastSearches").addClass("past-search").append("<button class= cityButton>" + storedCity + "</button>");    
-}
+// }
 
 buttonPress.on("click", function () {
-// set form data to local storage on button press
-// run function to search weather at the end of button press
+    if (userInput = "") {
+        alert("Please enter a city")
+    }
+    else {
+        userInput = $(this).siblings(".inputSearch").val().trim();
+        localStorage.setItem(localKey, userInput);
+        $(".pastSearches").addClass("past-search").append("<button class= cityButton>" + userInput + "</button>");
+    }
+    
+    searchWeather();
 })
 
 
-function searchWeather () {
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + apiKey + "&units=imperial";
+function searchWeather() {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + apiKey + "&units=imperial";
 
-    $.ajax ({
+    $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
+        // print current date using moment.js
+        var cityName = $(".forecast")
+        cityName.append("<p>" + userInput + "</p>");
+        cityName.append("<p>" + "Temperature: " + response.main.temp + "</p>");
+        cityName.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
+        cityName.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
     })
 }
+
+
+console.log(localKey);
+console.log(userInput);
