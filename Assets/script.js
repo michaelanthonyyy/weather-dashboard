aadc241a669bbe523d10380288dd12d5fb0d60ea
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-09 11:45:55 -0800

    added correct syntax for calling five day weather forcast inside of a for loop. dynamically created buttons from search on click function now properly calling forecast for respective cities

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,14 @@
 var apiKey = "9975a2d1ef4e7fb59bbba6eef797ea85"
 var buttonPress = $(".userSearch");
 var localKey = 0;
-var userInput = $(".inputSearch").val().trim();
+var userInput;
 var cityButton = $(".cityButton");
 var five = $(".fiveDay");
 
 
 buttonPress.on("click", function () {
-    if (userInput = "") {
+    userInput = $(".inputSearch").val().trim();
+    if (userInput === "") {
         alert("Please enter a city")
     }
     else {
@@ -16,10 +17,16 @@ buttonPress.on("click", function () {
         localStorage.setItem(localKey, userInput);
         $(".pastSearches").addClass("past-search").append("<button class= cityButton>" + userInput + "</button> <br>");
         localKey = localKey + 1
-        $(".inputSearch").empty();
+        $(".inputSearch").val("");
     }
-
     searchWeather();
+})
+
+$(document).on("click", ".cityButton", function () {
+    $(".forecast").empty();
+    userInput = ($(this).text());
+    searchWeather()
+
 })
 
 
@@ -50,23 +57,25 @@ function searchWeather() {
     })
 }
 
-function fiveDayForecast() { 
+function fiveDayForecast() {
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&units=imperial&appid=" + apiKey
-    
+
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        five.append("<p>" + "Temp: " + response.list[0].main.temp + "</p>");
-        five.append("<p>" + "Humidity: " + response.list[0].main.humidity + "% </p>");
+        for (i = 0; i < 5; i++) {
+            // five.append("<container class=> </container")
+            // var row = $("<div class=row> </div>")
+            // // row then five to column
+            five.append("<p>" + "Temp: " + response.list[i].main.temp + "</p>");
+            five.append("<p>" + "Humidity: " + response.list[i].main.humidity + "% </p>");
+        }
     })
 }
+// Change format of html to properly display five day weather forecast?
+// Possibly use card deck?
 
-cityButton.on("click", "button", function () {
-    $(".forecast").empty();
-    userInput.val($(this).text());
-    searchWeather()
-})
 
 // console.log(localKey);
 // console.log(userInput);
